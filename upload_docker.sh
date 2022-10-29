94a7d5f243ca9ba7aaf1f0633d5510317865441e
ZM <jan@janmarcan.com>
2022-10-29 22:02:44 +0200

    add upload to docker functionality

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=z3915/sklearn_app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username z3915
+docker tag sklearn_app:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
